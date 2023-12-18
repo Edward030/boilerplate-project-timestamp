c8b81c0d81529f03745904b04db37982e773731c
Replit user <>
2023-12-18 08:15:03 +0000

    v2

--- index.js
+++ index.js
@@ -24,6 +24,43 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+
+app.get("/api/:date?", function (req, res) {
+  let userDate = req.params.date;
+  /*
+  date1 = new Date(1451001600000);
+  date2 = new Date("1451001600000");
+  console.log("date1: ",date1,"date2: ", date2);
+  */
+  let date = parseInt(userDate, 10);
+  console.log(userDate, typeof(userDate), date);
+
+  // 如果日期參數是空的，使用當前時間
+  if (!userDate) {
+    userDate = new Date();
+  } 
+  else if (/^\d+$/.test(userDate)) {
+    userDate = new Date(parseInt(userDate, 10));
+  }
+  else {
+    userDate = new Date(userDate);
+  }
+  
+  console.log(userDate, userDate.getTime(), !isNaN(userDate.getTime()));
+  
+  if (!isNaN(userDate.getTime())) {
+    // 如果日期有效，返回對應的 JSON 對象
+    res.json({
+      unix: userDate.getTime(),
+      utc: userDate.toUTCString()
+    });
+  } else {
+    // 如果日期無效，返回包含錯誤消息的 JSON 對象
+    res.json({ error: "Invalid Date" });
+  }
+});
+
+/*
 // /api/date
 const someDay = new Date(1451001600000); 
 const formattedDate = someDay.toISOString().substr(0, 10);
@@ -43,6 +80,7 @@ app.get(`/api/${someDayFormat}`, (req, res) => {
     utc: someDay.toUTCString()
   });
 });
+*/
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
